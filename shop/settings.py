f2ad1243e752362d8b04dd3d945574c943c946f5
蒋洪波 <891893318@qq.com>
2024-02-05 19:04:13 +0800

    host修改

--- shop/settings.py
+++ shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3u$5zt+^t0k+!px+74t9qu_zt($-u+k9&-0v-=x24ynh*-&(l^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 import sys
